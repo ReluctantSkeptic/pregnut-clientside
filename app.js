f70007bdb8c2a220e3f4c431fb59fe7dd0d47eca
Yuriy Stasyuk <ystasyuk@gmail.com>
2018-02-18 16:10:03 -0800

    magic button that can filter sheet or workbook

--- app.js
+++ app.js
@@ -27,10 +27,24 @@ function switchView(sheetName) {
 
 }
 
+//4. Clear all filters
+function clearFilters() {
+    var sheet = viz.getWorkbook().getActiveSheet();
+    alert("clearing sheet:" + workbook.getActiveSheet().getName());
+    activeSheet.clearFilterAsync("FoodName");
+    } 
+
+//Print out your selection in html
+function print() {
+    document.getElementById("mySheet").innerHTML =  workbook.getActiveSheet().getName() ;
+    document.getElementById("myChoice").innerHTML = document.getElementById("FoodFinderInput").value;
+   //document.getElementById("myChoice").innerHTML = document.getElementById("selectFood").value;
+};
+
+
 //4. Switch to Finder and do chain call for food
 function foodFinderChain() {
     var selectedFood = document.getElementById("FoodFinderInput").value
-
     workbook.activateSheetAsync("FoodFinder")
       .then(function (newSheet) {
         activeSheet = newSheet;
@@ -38,17 +52,31 @@ function foodFinderChain() {
         });
     }; 
 
-//4. Clear all filters
-    function clearFilters() {
-        var sheet = viz.getWorkbook().getActiveSheet();
-        alert("clearing sheet:" + workbook.getActiveSheet().getName());
-        activeSheet.clearFilterAsync("FoodName");
-        } 
+// Filter a dashboard not sheet  //http://www.datablick.com/blog/tableau-js-api-101
+function filterDash() {
+    var selectedFood = document.getElementById("FoodFinderInput").value;
+    sheet=viz.getWorkbook().getActiveSheet();
 
-//Print out your selection in html
-    function print() {
-        document.getElementById("mySheet").innerHTML =  workbook.getActiveSheet().getName() ;
-        document.getElementById("myChoice").innerHTML = document.getElementById("FoodFinderInput").value;
-       
-        //document.getElementById("myChoice").innerHTML = document.getElementById("selectFood").value;
+    var dashboard, finderDash;
+    workbook.activateSheetAsync("FoodCalcDash")
+        .then(function (sheet) {
+        dashboard = sheet;
+        finderDash = dashboard.getWorksheets().get("FoodFinder");
+        return finderDash.applyFilterAsync("FoodName", selectedFood, tableau.FilterUpdateType.REPLACE);
+    }); 
+};
+
+// query which worksheets exist in dash
+function vizFilter(filterValue){
+    sheet=viz.getWorkbook().getActiveSheet();
+    if(sheet.getSheetType() === 'worksheet'){
+        sheet.applyFilterAsync("FoodName",filterValue,tableau.FilterUpdateType.REPLACE);
+    }
+    else{
+        worksheetArray = sheet.getWorksheets();
+        for(var i =0; i < worksheetArray.length; i++){
+            worksheetArray[i].applyFilterAsync("FoodName",filterValue,tableau.FilterUpdateType.REPLACE);
+            }
+        }
     };
+
