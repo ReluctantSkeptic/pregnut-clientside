a9a4b9a6c6c1a247aa58cb6742aa1f49423084ce
Yuriy Stasyuk <ystasyuk@gmail.com>
2018-02-23 12:32:19 -0800

    moving search to json query

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // 1 - Global Variable 
 var viz,workbook;
 
-
 // 2. Initialize the Tableau workbook to pull it into the html div container
 function initViz() {
     var containerDiv = document.getElementById("vizContainer")
@@ -21,44 +20,18 @@ function initViz() {
 };
 
 
-
 // 3. Switch to alternative sheets
 function switchView(sheetName) {
     var workbook = viz.getWorkbook();
     workbook.activateSheetAsync(sheetName);
-    print();
-
 };
-
-
 
 //4. Clear all filters
-function clearFilters() {
-    var sheet = viz.getWorkbook().getActiveSheet();
-    alert("clearing sheet:" + workbook.getActiveSheet().getName());
-    activeSheet.clearFilterAsync("FoodName");
-};
-
-
-
-// Print out your selection in html - for testing
-function print() {
-    document.getElementById("mySheet").innerHTML =  workbook.getActiveSheet().getName() ;
-    document.getElementById("myChoice").innerHTML = document.getElementById("FoodFinderInput").value;
-};
-
-
-
-//4. Switch to Finder Sheet and do chain call for food - only works on sheet, not dash
-function foodFinderChain() {
-    var selectedFood = document.getElementById("FoodFinderInput").value
-    workbook.activateSheetAsync("FoodFinder")
-      .then(function (newSheet) {
-        activeSheet = newSheet;
-        return activeSheet.applyFilterAsync("FoodName",selectedFood,tableau.FilterUpdateType.REPLACE);
-    });
-}; 
-
+// function clearFilters() {
+//     var sheet = viz.getWorkbook().getActiveSheet();
+//     alert("clearing sheet:" + workbook.getActiveSheet().getName());
+//     activeSheet.clearFilterAsync("FoodName");
+// };
 
 
 // 5. Filter Multi Execute 
@@ -66,7 +39,6 @@ function filterMulti() {
     var selectedFood = document.getElementById("FoodFinderInput").value;
     filterDash(selectedFood);
 };
-
 
 
 // 5.2 Filter a dashboard not sheet  //http://www.datablick.com/blog/tableau-js-api-101
@@ -79,14 +51,3 @@ function filterDash(mySelectedFood) {
         return finderDash.applyFilterAsync("FoodName", mySelectedFood , tableau.FilterUpdateType.REPLACE);
     }); 
 };
-
-
-
-
-
-
-function clearToolbar(){
-
-    document.getElementById("FoodFinderInput").setAttribute("style","height: 1px;");
-};
-
