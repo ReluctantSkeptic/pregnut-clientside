7f2ccdd324ef7fb49502f8be186e7bbfe9a7e14c
ReluctantSkeptic <ystasyuk@gmail.com>
2026-09-23 15:53:30 -0700

    Make homepage guide links descriptive

--- src/_data/sitemapData.js
+++ src/_data/sitemapData.js
@@ -2,6 +2,7 @@ const weeklyProtocol = require("./weeklyProtocol.js");
 const foodpages = require("./foodpages.js");
 
 // Advance these dates only when the corresponding page content changes substantially.
+const homepageLastmod = "2026-09-23";
 const blogHubLastmod = "2026-09-23";
 const foodPagesLastmod = "2026-09-23";
 
@@ -19,6 +20,9 @@ const paths = [
 ];
 
 module.exports = [...new Set(paths)].sort().map((path) => {
-  const lastmod = path === "/blog/" ? blogHubLastmod : path.startsWith("/food/") ? foodPagesLastmod : null;
+  let lastmod;
+  if (path === "/") lastmod = homepageLastmod;
+  else if (path === "/blog/") lastmod = blogHubLastmod;
+  else if (path.startsWith("/food/")) lastmod = foodPagesLastmod;
   return lastmod ? { path, lastmod } : { path };
 });
