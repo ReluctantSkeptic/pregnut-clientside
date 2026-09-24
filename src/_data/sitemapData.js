fc25190db54a8c7b15e9716265d2d6ff71407b61
ReluctantSkeptic <ystasyuk@gmail.com>
2026-09-24 15:56:12 -0700

    Link food safety and underlinked nutrient guides

--- src/_data/sitemapData.js
+++ src/_data/sitemapData.js
@@ -4,7 +4,7 @@ const foodpages = require("./foodpages.js");
 // Advance these dates only when the corresponding page content changes substantially.
 const homepageLastmod = "2026-09-23";
 const blogHubLastmod = "2026-09-24";
-const foodPagesLastmod = "2026-09-23";
+const foodPagesLastmod = "2026-09-24";
 
 const paths = [
   "/",
